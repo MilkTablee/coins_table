f3cd81f049f5906e6e4e07805869e53c49e5c2e3
MilkTablee <119823959+MilkTablee@users.noreply.github.com>
2024-07-03 16:28:12 +0100

    Implemented and styled Currency input and result rows

---
+++ src/ResultRow.tsx
@@ -0,0 +1,7 @@
+export default function ResultRow(){
+  return(
+    <div className="border min-h-12 border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2">
+      test row
+    </div>
+  );
+}
